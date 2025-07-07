9a80f02297e2d72e504e739a7385010a805f51fc
rajan98 <rajan.kum.sha@gmail.com>
2025-07-07 12:03:45 +0530

    Added new phone number as phone2

--- src/constants/app_constants.js
+++ src/constants/app_constants.js
@@ -1,7 +1,7 @@
 const APP_CONSTANTS = {
     company_name_title_case: "Indish",
     phone_number1: "(04) 5232 8901",
-    phone_number2: "",
+    phone_number2: "(02) 4031 5255",
     short_location: "6/115 Darby Street, Australia",
     full_address_line1: "6/115 Darby Street",
     full_address_line2: "Cooks Hill, Australia",
